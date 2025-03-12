80707c8c37203175a9c286c95dd9a958cad6dbe2
uniben2018 <uniben2018@gmail.com>
2025-03-12 05:00:08 +0000

    dev.nix

---
+++ dev.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_18
+    pkgs.python3
+    pkgs.python3Packages.pip
+  ];
+  ...
+}
--- telegram/telegram.py
+++ telegram/telegram.py
@@ -3,5 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 telegram_bot_token = os.getenv("TELEGRAM_BOT")
 
+print(telegram_bot_token);
+
 
 
